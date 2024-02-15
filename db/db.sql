6c4af827ac20ba79de2dba9cd588ee1e18f5666e
omarSuarezRodriguez <omar.suarez.rod@gmail.com>
2024-02-14 19:39:27 -0500

    1.37

--- db/db.sql
+++ db/db.sql
@@ -14,3 +14,65 @@ CREATE TABLE users(
     created_at TIMESTAMP(0) NOT NULL,
     updated_at TIMESTAMP(0) NOT NULL
 );
+
+
+
+USE udemy_delivery;
+
+CREATE TABLE roles (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(90) NOT NULL UNIQUE,
+    image VARCHAR(255) NULL,
+    route VARCHAR(180) NOT NULL,
+    create_at TIMESTAMP(0) NOT NULL,
+    updated_at TIMESTAMP(0) NOT NULL
+);
+
+INSERT INTO roles(
+	name,
+	route,
+    create_at,
+    updated_at
+)
+VALUES(
+	'RESTAURANTE',
+    '/restaurant/orders/list',
+    '2024-02-14',
+    '2024-02-14'
+);
+
+INSERT INTO roles(
+	name,
+	route,
+    create_at,
+    updated_at
+)
+VALUES(
+	'REPARTIDOR',
+    '/delivery/orders/list',
+    '2024-02-14',
+    '2024-02-14'
+);
+
+INSERT INTO roles(
+	name,
+	route,
+    create_at,
+    updated_at
+)
+VALUES(
+	'CLIENTE',
+    '/client/products/list',
+    '2024-02-14',
+    '2024-02-14'
+);
+
+CREATE TABLE user_has_roles(
+	id_user BIGINT NOT NULL,
+    id_rol BIGINT NOT NULL,
+    create_at TIMESTAMP(0) NOT NULL,
+    updated_at TIMESTAMP(0) NOT NULL,
+    FOREIGN KEY(id_user) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(id_rol) REFERENCES roles(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY(id_user, id_rol)
+);
